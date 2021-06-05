51d78261bf1401173b0f0a8b7826a7f29bd58a57
Manish Meganathan <757manish@gmail.com>
2021-06-05 23:09:13 +0530

    Add ability to convert GeoTIFFs to JPG
    - Added module convert.py
    - Added a function to convert a given GeoTIFF image into JPG using CV2

---
+++ geocore/convert.py
@@ -0,0 +1,16 @@
+import os
+import cv2
+
+def convert_GeoTIFF_TO_JPEG(geotiffpath: str, jpegpath: str):
+    if not geotiffpath.endswith(".tif"):
+        raise ValueError("Invalid path to GeoTIFF file. Must end with '.tif' extension.")
+
+    if not jpegpath.endswith(".jpg"):
+        raise ValueError("Invalid path to JPG file. Must end with '.jpg' extension.")
+
+    if not os.path.isfile(geotiffpath):
+        raise FileNotFoundError("Invalid path to GeoTIFF file. Does not exist!")
+
+    geotiff = cv2.imread(geotiffpath)
+    cv2.imwrite(jpegpath, geotiff, [int(cv2.IMWRITE_JPEG_QUALITY), 200])
+    return
