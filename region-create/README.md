c005e5d3a3a14104fe4cd98200bca3e000048633
Manish Meganathan <meganathan.manish@gmail.com>
2021-06-28 15:52:57 +0530

    Add code for region-create service
    - Added a requirements.txt file
    - Updated the README to indicate completion
    - Updated main.py file header
    - Updated the main.py main function for functionality as described in the README

--- region-create/README.md
+++ region-create/README.md
@@ -1,4 +1,5 @@
 # region-create
+- Status: Complete
 - This cloud function is responsible for handling creation events for documents in the **'regions'** collection of the Firestore Database
 - Responsible for sending a pubsub trigger to the **email-builder** for the 'new-region' runtime.
 - Responsible for sending a pubsub trigger to the **acquisition-builder** runtime.
--- region-create/main.py
+++ region-create/main.py
@@ -1,14 +1,16 @@
 """
 Terrascope Cloud
 
-service: region-create
-runtime: Python 3.9
-environment: Cloud Functions
+Google Cloud Platform
+Cloud Functions
 
+function: region-create
+runtime: python39
 trigger-event: providers/cloud.firestore/eventTypes/document.create 
 trigger-resource: projects/{projectID}/databases/(default)/documents/regions/{regionID}
 """
 import json
+import google.cloud.pubsub
 
 def log(severity: str, message: str, metadata: dict):
     """
@@ -24,21 +26,49 @@ def log(severity: str, message: str, metadata: dict):
 
 
 def main(event, context):
-    """doc"""
-    logmetadata = {"service": "region-event", "event": event}
+    """ Runtime Entrypoint """
+    logmetadata = {"service": "region-create", "event": event}
 
     try:
         regiondoc = event['value']['name']
+        gcpproject = regiondoc.split("/")[1]
         regiondoc = regiondoc.split('documents/')[-1]
-        docfields = event['value']['fields']
 
     except KeyError as e:
-        log("EMERGENCY", f"failed to parse event data: missing key - '{e}'", logmetadata)
-        return "Failure", 200
+        log("EMERGENCY", f"failed to parse event data. missing key - '{e}'", logmetadata)
+        return "error-acknowledge", 200
     except Exception as e:
-        log("ERROR", f"failed to parse event data: {e}", logmetadata)
-        return "Failure", 200
+        log("ALERT", f"failed to parse event data. {e}", logmetadata)
+        return "error-acknowldege", 200
 
-    logmetadata.update({"document-path": regiondoc})
+    logmetadata.update({"document-path": regiondoc, "gcp-project": gcpproject})
 
-    log("NOTICE", f"completed", logmetadata)
+    try:
+        publisher = google.cloud.pubsub.PublisherClient()
+        message = regiondoc.encode('utf-8')
+        
+    except Exception as e:
+        log("ALERT", f"failed to setup cloud pubsub client and topics. {e}.", logmetadata)
+        return "error-acknowledge", 200
+
+    try:
+        emailtopic = f'projects/{gcpproject}/topics/email-builds'
+        publisher.publish(topic=emailtopic, data=message, runtime="new-region")
+        
+    except Exception as e:
+        log("ALERT", f"failed to trigger the 'new-region' email-build. {e}.", logmetadata)
+    else:
+        log("DEBUG", f"succesfully triggered 'new-region' email-build.", logmetadata)
+
+    try:
+        acqtopic = f'projects/{gcpproject}/topics/acquisition-builds'
+        publisher.publish(topic=acqtopic, data=message)
+
+    except Exception as e:
+        log("ALERT", f"failed to trigger the acquisition-build. {e}.", logmetadata)
+        return "error-acknowledge", 200
+    else:
+        log("DEBUG", f"succesfully triggered the acquisition-build.", logmetadata)
+
+    log("INFO", f"completed region-create runtime.", logmetadata)
+    return "success-acknowledge", 200
