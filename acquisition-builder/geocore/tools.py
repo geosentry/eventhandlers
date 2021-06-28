a79c64aa500ef3fe939778028a5b91ab4808ad78
Manish Meganathan <meganathan.manish@gmail.com>
2021-06-29 00:05:46 +0530

    Update the GeoCore Package
    - Updated the geocore package within the acquisition-builder directory
    - Moved the conversion.py functions into a new file tools.py
    - Added functions for geometry generation and image identifier retrieval

--- acquisition-builder/geocore/tools.py
+++ acquisition-builder/geocore/tools.py
@@ -1,8 +1,10 @@
 """
 Terrascope GeoCore Library
 """
+import ee
 import os
 import cv2
+import json
 
 def convert_GeoTIFF_TO_JPEG(geotiffpath: str, jpegpath: str):
     if not geotiffpath.endswith(".tif"):
@@ -17,3 +19,14 @@ def convert_GeoTIFF_TO_JPEG(geotiffpath: str, jpegpath: str):
     geotiff = cv2.imread(geotiffpath)
     cv2.imwrite(jpegpath, geotiff, [int(cv2.IMWRITE_JPEG_QUALITY), 200])
     return
+
+def generate_geometry(geojson: str) -> ee.Geometry:
+    """ """
+    geodata = json.loads(geojson)
+    coordinates = geodata['features'][0]['geometry']['coordinates'][0]
+    geometry = ee.Geometry.Polygon(coordinates)
+    return geometry
+
+def generate_image_identifier(image: ee.Image) -> str:
+    """"""
+    return f"COPERNICUS/S2_SR/{image.id().getInfo()}"
