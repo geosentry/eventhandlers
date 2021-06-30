37c0694fe667e7fa217555251fb0218e4774da0a
Manish Meganathan <meganathan.manish@gmail.com>
2021-06-30 14:14:57 +0530

    Update asset-create service
    - Updated the log flow of the asset-create service.

--- acquisition-builder/main.py
+++ acquisition-builder/main.py
@@ -1,15 +1,9 @@
 """
 Terrascope Cloud
 
-Google Cloud Platform
-Cloud Functions
+Google Cloud Platform - Cloud Functions
 
-function: acquisition-builder
-runtime: python39
-trigger-topic: acquisition-builds
-service-account permissions:
-- Secret Accesor
-- Cloud Datastore User
+acquisition-builder service
 """
 import ee
 import json
@@ -17,16 +11,18 @@ import base64
 import geocore
 import google.cloud.firestore as firestore
 
-def log(severity: str, message: str, metadata: dict):
+servicename = "acquisition-builder"
+
+def log(severity: str, message: str, trace: list):
     """
     A helper function that logs a given message to Cloud Logging as a structured log 
     given that it is called within a Cloud Run/Function Service. The function accepts 
-    a log severity string, a log message string and a log metadata dictionary.
+    a log severity string, a log message string and a a list of trace log strings.
 
     Accepted log severity values are - EMERGENCY, ALERT, CRITICAL, ERROR, WARNING, NOTICE, INFO, DEBUG and DEFAULT.
     Refer to https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#logseverity for more information.
     """
-    logentry = dict(severity=severity, message=message, **metadata)
+    logentry = dict(severity=severity, message=message, trace=trace, service=servicename)
     print(json.dumps(logentry))
 
 def parse_pubsub_event(event: dict):
@@ -58,67 +54,85 @@ def parse_pubsub_event(event: dict):
 
 def main(event, context):
     """ Cloud Functions Entrypoint """
-    logmetadata = {"service": "acquisition-builder"}
+    logtraces = ["execution started."]
 
     try:
         docpath, _ = parse_pubsub_event(event)
 
     except Exception as e:
-        log("EMERGENCY", f"failed to parse pubsub event data. {e}", logmetadata)
+        logtraces.append(f"execution broke - could not read pubsub event data. {e}")
+        log("EMERGENCY", f"system error.", logtraces)
         return "error-acknowldege", 200
 
-    logmetadata.update({"document-path": docpath})
+    logtraces.append(f"event data read. document - {docpath}.")
 
     try:
         db = firestore.Client()
+        logtraces.append(f"firestore client initialized.")
+
         geocore.initialize(db.project)
+        logtraces.append(f"earth engine session initialized.")
         
     except RuntimeError as e:
-        log("ALERT", f"failed to setup cloud clients. {e}.", logmetadata)
-        return "error-acknowledge", 200
-    except Exception as e:
-        log("ALERT", f"failed to setup cloud clients. unable to initialize a firestore client. {e}.", logmetadata)
+        logtraces.append(f"execution broke - could not initialize earth engine session. {e}")
+        log("ALERT", f"system error.", logtraces)
         return "error-acknowledge", 200
 
-    logmetadata.update({"gcp-project": db.project})
+    except Exception as e:
+        logtraces.append(f"execution broke - could not initialize firestore client. {e}")
+        log("ALERT", f"system error.", logtraces)
+        return "error-acknowledge", 200
 
     try:
         docref = db.document(docpath)
         docdata = docref.get().to_dict()
 
-        active = docdata["active"]
+        logtraces.append(f"region document data retrieved.")
+
         geojson = docdata["geojson"]
+        geometry = geocore.spatial.generate_geometry(geojson)
 
-        if not active:
-            log("INFO", "region has been deactivated", logmetadata)
-            return "termination-acknowledge", 200
-
-    except KeyError as e:
-        log("CRITICAL", f"failed to read region document data. missing key {e}")
+    except KeyError:
+        logtraces.append(f"execution broke - could not read geojson field from region document.")
+        log("ERROR", f"runtime error.", logtraces)
         return "error-acknowledge", 200
+
+    except RuntimeError as e:
+        logtraces.append(f"execution broke - could not generate geometry from region geojson. {e}")
+        log("ERROR", f"runtime error.", logtraces)
+        return "error-acknowledge", 200
+
     except Exception as e:
-        log("ALERT", f"failed to read region document data. {e}.", logmetadata)
+        logtraces.append(f"execution broke - could not retrieve region document data from firestore. {e}")
+        log("ERROR", f"runtime error.", logtraces)
         return "error-acknowledge", 200
+
+    logtraces.append(f"region geometry generated.")
 
     try:
-        geometry = geocore.spatial.generate_geometry(geojson)
         acqdate = docdata.get("next_acquisition")
-
         if not acqdate:
             acqdate = geocore.acquisition.generate_latest_date(geometry)
+            logtraces.append(f"new region. acquisition date generated.")
         else:
             acqdate = geocore.temporal.generate_date_googledate(acqdate)
 
-        acqimage = geocore.acquisition.generate_latest_image(acqdate, geometry)
+        logtraces.append(f"acquisition date determined. date - {acqdate.isoformat()}")
 
+        acqimage = geocore.acquisition.generate_latest_image(acqdate, geometry)
         if not acqimage:
-            log("INFO", "region has no available acquisitions", logmetadata)
+            logtraces.append(f"execution terminated - region has no available acquisitions.")
+            log("INFO", "runtime terminated.", logtraces)
             return "termination-acknowledge", 200
 
+        logtraces.append(f"acquisition image found")
+
         acqimageid = geocore.acquisition.generate_image_identifier(acqimage)
+        logtraces.append(f"acquisition image ID retrieved. imageID - {acqimageid}.")
 
     except Exception as e:
-        log("ALERT", f"failed to find latest acquisition for the region. {e}.", logmetadata)
+        logtraces.append(f"execution broke - could not find latest acquisition for region. {e}")
+        log("ERROR", f"runtime error.", logtraces)
         return "error-acknowledge", 200
 
     try:
@@ -129,37 +143,48 @@ def main(event, context):
         }, merge=True)
 
     except Exception as e:
-        log("ALERT", f"failed to update the region document. {e}.", logmetadata)
+        logtraces.append(f"execution broke - could not update region document. {e}")
+        log("CRITICAL", f"runtime error.", logtraces)
         return "error-acknowledge", 200
+
+    logtraces.append(f"region document updated.")
 
     # CLOUD PROBABILITY DETECTION RUNTIME
 
     try:
+        assets = ["truecolor", "ndvi"]
         tcimage = geocore.spectral.generate_TrueColor(acqimage).clip(geometry)
         ndviimage = geocore.spectral.generate_NDVI(acqimage).clip(geometry)
 
     except Exception as e:
-        log("ALERT", f"failed to generate acquisition asset images. {e}.", logmetadata)
+        logtraces.append(f"execution broke - could not generate acquisition asset images. {e}")
+        log("ERROR", f"runtime error.", logtraces)
         return "error-acknowledge", 200
+
+    logtraces.append(f"asset images generated.")
 
     try:
         acqID = acqdate.isoformat().split("T")[0]
-        assets = ["truecolor", "ndvi"]
-        assetpaths = {key: f"{docpath}/{acqID}/{key}" for key in assets}
-
+        logtraces.append(f"acquisition ID generated. acquistion ID - {acqID}")
+        
         # export runtime (temporary) - needs to accomodate a variable number of generations
-        tcexport = geocore.export.export_image_asset(tcimage, geometry, assetpaths["truecolor"])
+        tcexport = geocore.export.export_image_asset(tcimage, geometry, f"{docpath}/{acqID}/truecolor")
         tcexport.start()
 
-        ndviexport = geocore.export.export_image_asset(ndviimage, geometry, assetpaths["ndvi"])
+        ndviexport = geocore.export.export_image_asset(ndviimage, geometry, f"{docpath}/{acqID}/ndvi")
         ndviexport.start()
 
     except ee.EEException as e:
-        log("ALERT", f"failed to export acquisition asset images. earth engine error. {e}.", logmetadata)
+        logtraces.append(f"execution broke - could not export asset images. earth engine error. {e}")
+        log("CRITICAL", f"runtime error.", logtraces)
         return "error-acknowledge", 200
+
     except Exception as e:
-        log("ALERT", f"failed to export acquisition asset images. runtime error. {e}.", logmetadata)
+        logtraces.append(f"execution broke - could not export asset images. runtime error. {e}")
+        log("ERROR", f"runtime error.", logtraces)
         return "error-acknowledge", 200
+
+    logtraces.append(f"asset images exported.")
 
     try:
         acqdoc = docref.collection("acquisitions").document(acqID)
@@ -167,29 +192,17 @@ def main(event, context):
             "image": acqimageid,
             "timestamp": geocore.temporal.generate_googledate_date(acqdate),
             "subscription": "farm-analytics",
-            "asset": {
-                "bucket": "terrascope-assets",
-                "completion": {key: False for key in assets},
-                "path": assetpaths
-            },
-            "report": {
-                "bucket": "terrascope-reports",
-                "completion": False,
-                "path": "regions/regid/acqid.pdf"
-            }            
+            "asset-bucket": "terrascope-assets",
+            "asset-paths": {key: None for key in assets},
+            "report-bucket": "terrascope-reports",
+            "report-path": None,       
         })
 
     except Exception as e:
-        log("ALERT", f"failed to create acquisition document. {e}.", logmetadata)
+        logtraces.append(f"execution broke - could not create acquisition document. {e}")
+        log("CRITICAL", f"runtime error.", logtraces)
         return "error-acknowledge", 200
     
-    log("INFO", f"completed acquisition-builder runtime.", logmetadata)
+    logtraces.append("execution complete.")
+    log("INFO", f"runtime complete.", logtraces)
     return "success-acknowledge", 200
-
-if __name__ == "__main__":
-    event = {
-        "data": base64.b64encode("regions/testreg".encode("utf-8")),
-        "attributes": {}
-    }
-
-    main(event, 0)
