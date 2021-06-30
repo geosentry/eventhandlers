7ecdc9ec54f9719653f14b752f5467bf87ab4b40
Manish Meganathan <meganathan.manish@gmail.com>
2021-06-30 17:06:29 +0530

    Move the PNG asset handler runtime to the pnglib module
    - Added documentation for the other functions in the pnglib module

--- asset-create/assethandler/pnglib.py
+++ asset-create/assethandler/pnglib.py
@@ -5,7 +5,7 @@ The pnglib module contains functions to handle PNG assets triggers
 """
 
 def generate_assetid(filename: str):
-    """doc"""
+    """ A function that returns an asset ID for a given filename. """
     try:
         return filename.split("/")[-1].split(".")[0]
 
@@ -13,7 +13,7 @@ def generate_assetid(filename: str):
         raise NameError(e)
 
 def generate_assetdoc(filename: str):
-    """doc"""
+    """ A function that returns an asset document path for a given filename. """
     try:
         pieces = filename.split("/")
         assettype = pieces[0]
@@ -29,3 +29,63 @@ def generate_assetdoc(filename: str):
         raise TypeError(e)
     except Exception as e:
         raise NameError(e)
+
+def handle_png(filename: str, logtraces: list):
+    """
+    A handler function that performs the asset handle runtime for PNG assets.
+
+    Adds the path to the file to the corresponding Firestore document and then 
+    checks if that document has all the asset paths assigned, and if it does, 
+    sends a PubSub message to the trigger the appropriate PDF build runtime.
+    """
+    from google.cloud import firestore
+
+    logtraces.append("started PNG asset handler runtime")
+
+    try:
+        docpath = generate_assetdoc(filename)
+        logtraces.append(f"asset document path generated. docpath - {docpath}")
+
+        db = firestore.Client()
+        logtraces.append(f"firestore client initialized")
+
+        docref = db.document(docpath)
+        docsnap = docref.get()
+
+        if not docsnap.exists:
+            logtraces.append(f"execution broke - could not find asset document on database.")
+            return "CRITICAL", "runtime error.", "error-acknowledge", logtraces
+            
+    except NameError as e:
+        logtraces.append(f"execution broke - could not generate asset document path from filename. {e}")
+        return "ERROR", "runtime error.", "error-acknowledge", logtraces
+
+    except TypeError as e:
+        logtraces.append(f"execution terminated - triggered by unsupported asset type - {e}")
+        return "INFO", "runtime terminated.", "termination-acknowledge", logtraces
+
+    except Exception as e:
+        logtraces.append(f"execution broke - could not initialize firestore client. {e}")
+        return "ALERT", "system error.", "error-acknowledge", logtraces
+
+    logtraces.append(f"asset document path valudated.")
+
+    try:
+        assetid = generate_assetid(filename)
+        docref.set({f"asset-paths": {assetid: filename}}, merge=True)
+
+    except NameError as e:
+        logtraces.append(f"execution broke - could not generate asset ID from filename. {e}")
+        return "ERROR", "runtime error.", "error-acknowledge", logtraces
+        
+    except Exception as e:
+        logtraces.append(f"execution broke - could not update asset document. {e}")
+        return "ALERT", "system error.", "error-acknowledge", logtraces
+    
+    logtraces.append(f"asset document updated")
+    
+    # check if all document assets have been generated.
+    # send pubsub trigger to pdf-builds if true 
+
+    logtraces.append("ended PNG asset handler runtime")
+    return "INFO", "PNG runtime complete", "success-acknowledge", logtraces
--- asset-create/main.py
+++ asset-create/main.py
@@ -22,58 +22,6 @@ def log(severity: str, message: str, trace: list):
     logentry = dict(severity=severity, message=message, trace=trace, service=servicename)
     print(json.dumps(logentry))
 
-def handle_png(filename: str, logtraces: list):
-    from google.cloud import firestore
-
-    logtraces.append("started PNG asset handler")
-
-    try:
-        docpath = assethandler.pnglib.generate_assetdoc(filename)
-        logtraces.append(f"asset document path generated. docpath - {docpath}")
-
-        db = firestore.Client()
-        logtraces.append(f"firestore client initialized")
-
-        docref = db.document(docpath)
-        docsnap = docref.get()
-
-        if not docsnap.exists:
-            logtraces.append(f"execution broke - could not find asset document on database.")
-            return "CRITICAL", "runtime error.", "error-acknowledge", logtraces
-            
-    except NameError as e:
-        logtraces.append(f"execution broke - could not generate asset document path from filename. {e}")
-        return "ERROR", "runtime error.", "error-acknowledge", logtraces
-
-    except TypeError as e:
-        logtraces.append(f"execution terminated - triggered by unsupported asset type - {e}")
-        return "INFO", "runtime terminated.", "termination-acknowledge", logtraces
-
-    except Exception as e:
-        logtraces.append(f"execution broke - could not initialize firestore client. {e}")
-        return "ALERT", "system error.", "error-acknowledge", logtraces
-
-    logtraces.append(f"asset document path valudated.")
-
-    try:
-        assetid = assethandler.pnglib.generate_assetid(filename)
-        
-        #TODO - define asset path and check update runtime
-        docref.set({f"assets.path.{assetid}": filename}, merge=True)
-
-    except NameError as e:
-        logtraces.append(f"execution broke - could not generate asset ID from filename. {e}")
-        return "ERROR", "runtime error.", "error-acknowledge", logtraces
-        
-    except Exception as e:
-        logtraces.append(f"execution broke - could not update asset document. {e}")
-        return "ALERT", "system error.", "error-acknowledge", logtraces
-    
-    logtraces.append(f"asset document updated")
-
-    # check if all document assets have been generated.
-    # send pubsub trigger to pdf-builds if true
-
 
 def main(event, context):
     """ Cloud Functions Entrypoint """
@@ -83,13 +31,13 @@ def main(event, context):
         bucket = event["bucket"]
         filename = event["name"]
         contenttype = event["contentType"]
-
-        logtraces.append(f"event data read. bucket - {bucket}. filename - {filename}.")
     
     except KeyError as e:
-        logtraces.append(f"execution broke - could not read event data. missing key {e}")
+        logtraces.append(f"execution broke - could not read bucket event data. missing key {e}")
         log("EMERGENCY", f"system error.", logtraces)
         return "error-acknowledge", 200
+
+    logtraces.append(f"event data read. bucket - {bucket}. filename - {filename}.")
 
     try:
         if contenttype == "image/tiff":
@@ -100,7 +48,11 @@ def main(event, context):
             return response, 200
 
         elif contenttype == "image/png":
-            return "", 200
+            severity, message, response, logtraces = assethandler.pnglib.handle_png(filename, logtraces)
+
+            logtraces.append("execution complete")
+            log(severity, message, logtraces)
+            return response, 200
         
         else:
             logtraces.append(f"execution terminated - triggered by unsupported object of type - {contenttype}.")
@@ -111,14 +63,3 @@ def main(event, context):
         logtraces.append(f"execution broke - could not run {contenttype} runtime. {e}")
         log("ERROR", f"runtime error.", logtraces)
         return "error-acknowledge", 200
-
-
-
-if __name__ == "__main__":
-    event = {
-        "bucket": "terrascope-assets",
-        "contentType": "image/tiff",
-        "name": "regions/testreg/2021-06-24/truecolor.tif"
-    }
-
-    main(event, 0)
