d68d91eb74ee45dfe7b83090898ce67394aea2eb
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-11 21:50:33 +0530

    Update region-create runtime
    - Added new deps
    - Moved event structs and helpers to a new file
    - Project ID and Region are extracted from environment variables
    - Added http runtimes for the reshape service
    - Added service directory runtimes for the service resolving
    - Added firestore runtimes to update the region document

--- region-create/function.go
+++ region-create/function.go
@@ -8,53 +8,25 @@ region-create service
 package function
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
-	"strings"
-	"time"
+	"io/ioutil"
+	"net/http"
+	"os"
 
-	"cloud.google.com/go/pubsub"
+	metadata "cloud.google.com/go/compute/metadata"
+	firestore "cloud.google.com/go/firestore"
+	pubsub "cloud.google.com/go/pubsub"
+	servicedirectory "cloud.google.com/go/servicedirectory/apiv1"
+
+	sdpb "google.golang.org/genproto/googleapis/cloud/servicedirectory/v1"
 )
 
-// A structure that represents a generic Region Document
-// Only implements fields relevant to this service.
-type RegionDoc struct {
-	Type struct {
-		StringValue string `json:"stringValue"`
-	} `json:"type"`
-
-	GeoJSON struct {
-		StringValue string `json:"stringValue"`
-	} `json:"geojson"`
-}
-
-// A structure that reprsents a Document Snapshot
-// Specification: https://firebase.google.com/docs/firestore/reference/rest/v1beta1/projects.databases.documents
-type FirestoreDocument struct {
-	Name       string    `json:"name"`
-	Fields     RegionDoc `json:"fields"`
-	CreateTime time.Time `json:"createTime"`
-	UpdateTime time.Time `json:"updateTime"`
-}
-
-// A structure that represents a Cloud Firestore Event
-// Specification: https://cloud.google.com/functions/docs/calling/cloud-firestore#event_structure
-type FirestoreEvent struct {
-	Value      FirestoreDocument `json:"value"`
-	OldValue   FirestoreDocument `json:"oldValue"`
-	UpdateMask struct {
-		FieldPaths []string `json:"fieldPaths"`
-	} `json:"updateMask"`
-}
-
-func (event *FirestoreEvent) getDocPath() string {
-	splitpath := strings.Split(event.Value.Name, "documents/")
-	return splitpath[len(splitpath)-1]
-}
-
-func (event *FirestoreEvent) getProjectID() string {
-	splitpath := strings.Split(event.Value.Name, "/")
-	return splitpath[1]
+type ReshapeResponse struct {
+	Bounds []float32          `json:"bounds"`
+	Areas  map[string]float32 `json:"areas"`
 }
 
 // Cloud Functions Entrypoint
@@ -64,16 +36,124 @@ func Main(ctx context.Context, event FirestoreEvent) error {
 
 	// Retrieve the document path from the event data
 	docpath := event.getDocPath()
-	// Retrieve the project ID from the event data
-	project := event.getProjectID()
 	log.addtrace(fmt.Sprintf("event data read. docpath - %s.", docpath))
 
-	// TODO: read geojson field
-	// TODO: make http request to the geocore API's reshape endpoint
-	// TODO: update the region document geojson
+	// Retrieve the project ID from the env variables
+	project := os.Getenv("GCP_PROJECT")
+	// Retrieve the project region from the env variables
+	projectregion := os.Getenv("GCP_REGION")
+
+	// Create a Firestore Client
+	dbclient, err := firestore.NewClient(ctx, project)
+	if err != nil {
+		// Log and exit
+		log.addtrace("execution broke - could not initialize firestore client.")
+		log.flush("ALERT", "system error")
+		return nil
+	}
+	// Defer the closing of the Firestore Client
+	defer dbclient.Close()
+	log.addtrace("firestore client initialized.")
+
+	// Construct a document reference to the Region
+	regiondoc := dbclient.Doc(docpath)
+	// Create a slice of Firestore updates and initialize
+	// it with the Delete sentinel for the geojson field
+	updates := []firestore.Update{{Path: "geojson", Value: firestore.Delete}}
+
+	// Create a Service Directory Lookup Client
+	resolver, err := servicedirectory.NewLookupClient(ctx)
+	if err != nil {
+		// Log and exit
+		log.addtrace("execution broke - could not initialize service lookup client.")
+		log.flush("ALERT", "system error")
+		return nil
+	}
+	// Defer the closing of the Firestore Client
+	defer resolver.Close()
+	log.addtrace("service lookup client initialized.")
+
+	// Construct the service name for the 'geocore-spatio' service
+	servicename := fmt.Sprintf("projects/%s/locations/%s/namespaces/geocore/services/geocore-spatio", project, projectregion)
+	// Create a resolve request for the 'geocore-spatio' service
+	resolverequest := &sdpb.ResolveServiceRequest{Name: servicename}
+	// Resolve the service and obtain its information
+	resolveresponse, err := resolver.ResolveService(ctx, resolverequest)
+	if err != nil {
+		// Log and exit
+		log.addtrace("execution broke - could not resolve the geocore-spatio service.")
+		log.flush("ALERT", "system error")
+		return nil
+	}
+	log.addtrace("geocore-spatio service resolved.")
+
+	// Retrieve the URL of the 'geocore-spatio' service from its annotations
+	url := resolveresponse.Service.Annotations["url"]
+	// Create the URL for the reshape endpoint of the 'geocore-spatio' service.
+	reshapeURL := fmt.Sprintf("%s/reshape", url)
+	// Create the URL for the geocode endpoint of the 'geocore-spatio' service.
+	//geocodeURL := fmt.Sprintf("%s/geocode", url)
+
+	log.addtrace("service URLs generated.")
+
+	// Retrieve the data of the 'geojson' field from the event data
+	geojson := event.Value.Fields.GeoJSON.StringValue
+	// Construct the request body with the geojson data to reshape
+	var requestbody = []byte(fmt.Sprintf(`{"geojson": %s}`, geojson))
+	// Construct the request object
+	request, err := http.NewRequest("POST", reshapeURL, bytes.NewBuffer(requestbody))
+	if err != nil {
+		log.addtrace("")
+	}
+	log.addtrace("reshape request body generated.")
+
+	// Set the Content-Type and User-Agent headers of the request
+	request.Header.Set("User-Agent", "region-create cloud function")
+	request.Header.Set("Content-Type", "application/json")
+
+	// Retrieve authentication token from the GCP Compute Metadata API for the reshape service URL
+	reshapetokenURL := fmt.Sprintf("/instance/service-accounts/default/identity?audience=%s", reshapeURL)
+	reshapetoken, err := metadata.Get(reshapetokenURL)
+	if err != nil {
+		log.addtrace("")
+	}
+	// Set the Authorization Header of the request
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", reshapetoken))
+
+	log.addtrace("reshape request headers generated.")
+
+	// Create a HTTP Client
+	netclient := &http.Client{}
+	// Perform the HTTP request
+	response, err := netclient.Do(request)
+	if err != nil {
+		log.addtrace("")
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.addtrace("")
+	}
+
+	var reshaperesponse ReshapeResponse
+	json.Unmarshal(body, &reshaperesponse)
+
+	// add bounds and area data to region document
+	updates = append(updates, firestore.Update{Path: "bounds", Value: reshaperesponse.Bounds})
+	updates = append(updates, firestore.Update{Path: "namenew", Value: reshaperesponse.Areas})
+
+	// TODO: geocoding
+	// construct request object with bounds data
+	// retrieve idtoken from computemeta api for geocode and set auth header
+	// make HTTP request to geocode and collect response data
+	// add geocode data to the region document
+
+	// Update the region document to firestore.
+	regiondoc.Update(ctx, updates)
 
 	// Create a PubSub Client
-	client, err := pubsub.NewClient(ctx, project)
+	pusbsubclient, err := pubsub.NewClient(ctx, project)
 	if err != nil {
 		// Log and exit
 		log.addtrace("execution broke - could not initialize pubsub client.")
@@ -81,11 +161,11 @@ func Main(ctx context.Context, event FirestoreEvent) error {
 		return nil
 	}
 	// Defer the closing of the PubSub Client
-	defer client.Close()
+	defer pusbsubclient.Close()
 	log.addtrace("pubsub client initialized.")
 
 	// Create a PubSub Topic Handler
-	topic := client.Topic("taskbuilds")
+	topic := pusbsubclient.Topic("taskbuilds")
 	log.addtrace("pubsub topic created.")
 
 	// Declare a buildflow string
